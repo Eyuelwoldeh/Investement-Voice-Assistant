0fc27a1580eb0dc0bd2b9f80291ace525012f5e1
eyuelwoldehanna <eyuelwoldehanna@gmail.com>
2025-01-26 18:32:26 -0600

    added context

--- main.py
+++ main.py
@@ -1,13 +1,31 @@
 import asyncio
 from dotenv import load_dotenv
-from livekit.agents import AutoSubscrinbe, JobContext, WorkerOptions, cli, llm,
+from livekit.agents import AutoSubscribe, JobContext, WorkerOptions, cli, llm,
 from livekit.agents.voice_assistant import VoiceAssistant
 from livekit.plugins import openai, silero
 
 load_dotenv()
 
 async def entryppoint(ctx: JobContext):
-    pass
+    initial_ctx = llm.ChatContext().append(
+        role="system",
+        text=(
+            "You are a voice assistant created by LiveKit. Your interface with users will be voice. "
+            "You should use short and concise responses, and avoid usage of unpronouncable punctuation."
+        ),
+    )
+
+    await ctx.connect(auto_subscribe=AutoSubscribe.AUDIO_ONLY)
+
+    assistant = VoiceAssistant(
+        vad=silero.VAD.load(),
+        stt=openai.STT(),
+        llm=openai.LLM(),
+        tts=openai.TTS(),
+        chat_ctx=initial_ctx
+    )
+
+    assistant.start(ctx.room)
 
 if __name__ == "__main__":
     cli.run_app(WorkerOptions(entrypoint_fnc=entryppoint))
